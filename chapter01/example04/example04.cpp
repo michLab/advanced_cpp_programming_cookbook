5ac70632848dc057f81904be91a8ba94561bc94f
Michal Labowski <m.labowski@wb.com.pl>
2024-02-10 17:31:18 +0100

    feat: Add chapter01/example04

---
+++ chapter01/example04/example04.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int universe() {
+    auto i = new int;
+    int the_answer;
+    return the_answer;
+}
+
+int main() {
+    std::cout << universe() << std::endl;
+    return 0;
+}
